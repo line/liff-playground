034bfdf0f67af9bf98bce7c0acb0829f22f6a2ff
dyoshikawa <dyoshikawa1993@gmail.com>
2022-05-02 23:16:02 +0900

    configure https by devcert

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,25 @@
-module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        crypto: false,
-        stream: false,
-      };
-      return webpackConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const devcert = require('devcert')
+
+module.exports = async () => {
+  const { key, cert } = await devcert.certificateFor('localhost')
+
+  return {
+    webpack: {
+      configure: (webpackConfig) => {
+        webpackConfig.resolve.fallback = {
+          crypto: false,
+          stream: false,
+        }
+        return webpackConfig
+      },
     },
-  },
-};
+    devServer: (devServerConfig) => {
+      devServerConfig.https = {
+        key,
+        cert,
+      }
+      return devServerConfig
+    },
+  }
+}
