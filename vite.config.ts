782eb2bc98e92e0f7fcc8d52a3e97fdcc27f49e9
dyoshikawa <dyoshikawa1993@gmail.com>
2022-05-24 21:48:26 +0900

    Configure https by devcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { UserConfigExport, ServerOptions } from 'vite'
+import react from '@vitejs/plugin-react'
+import { certificateFor } from 'devcert'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-});
+export default async (): Promise<UserConfigExport> => {
+  let server: ServerOptions | undefined = undefined
+  // if (process.env.HTTPS === 'true') {
+  const { key, cert } = await certificateFor('localhost')
+  server = {
+    https: {
+      key,
+      cert,
+    },
+  }
+  // }
+
+  return {
+    plugins: [react()],
+    server,
+  }
+}
